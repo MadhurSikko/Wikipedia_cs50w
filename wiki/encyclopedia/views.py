df3df117905ed525c45403dc7b2fba35cd9aa990
MadhurSikko <madhur.dagar.s@gmail.com>
2023-07-17 15:58:10 +0530

    Entry page and index page linking

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
+
+def markdownToHTML(name):
+    content = markdown2.markdown(util.get_entry(name))
+    return content
 
 
 def index(request):
@@ -8,3 +12,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, name):
+    if name not in util.list_entries():
+        return render(request, "encyclopedia/error.html", {
+            "entry": False,
+        })
+    else:
+        content = markdownToHTML(name)
+        return render(request, "encyclopedia/page.html", {
+            "content": content,
+            "title": name,
+        })
