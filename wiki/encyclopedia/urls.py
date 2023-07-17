72b45dc70d18ea0b1678bd2341b6c5c3c1c7b6f0
MadhurSikko <madhur.dagar.s@gmail.com>
2023-07-17 23:42:29 +0530

    added random feature

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.page, name="page")
+    path("wiki/<str:name>", views.page, name="page"),
+    path("newPage", views.newPage, name="newPage"),
+    path("randomPage", views.randomPage, name="randomPage"),
+    #path("editPage/<str:name>", views.editPage, name="editPage")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django import forms
 import markdown2
 from . import util
+from django.http import HttpResponseRedirect
+import random
+
+class newPageForm(forms.Form):
+    title =  forms.CharField()
+    body = forms.CharField(widget=forms.Textarea)
 
 def markdownToHTML(name):
     content = markdown2.markdown(util.get_entry(name))
     return content
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -23,3 +30,27 @@ def page(request, name):
             "content": content,
             "title": name,
         })
+    
+def newPage(request):
+    if request.method == "POST":
+        form = newPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            body = form.cleaned_data["body"]
+            
+            if title not in util.list_entries():
+                util.save_entry(title, body)
+                return HttpResponseRedirect("wiki/"+title)
+            else:
+                return render(request, "encyclopedia/newPage.html", {
+                    "form": newPageForm(),
+                    "Error": True,
+                })
+            
+    return render(request, "encyclopedia/newPage.html", {
+        "form": newPageForm(),
+    })
+
+def randomPage(request):
+    list = random.choice(util.list_entries())
+    return HttpResponseRedirect("wiki/"+list)
