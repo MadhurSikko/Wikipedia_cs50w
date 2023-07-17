cca2478bb536980f3b217db8310af01d346745e7
MadhurSikko <madhur.dagar.s@gmail.com>
2023-07-17 16:01:36 +0530

    fixed the links in markdown file

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("WIKI/<str:name>", views.page, name="page")
+    path("wiki/<str:name>", views.page, name="page")
 ]
